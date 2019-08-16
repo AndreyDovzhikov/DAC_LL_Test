0dca9295327814fed43e1aa98a280237538bc78b
Andrew <43615070+AndreyDovzhikov@users.noreply.github.com>
2019-08-16 16:52:25 +0300

    Add files via upload

---
+++ MDK-ARM/RTE/_DAC_LL_Test/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DAC_LL_Test' 
+ * Target:  'DAC_LL_Test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f2xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
